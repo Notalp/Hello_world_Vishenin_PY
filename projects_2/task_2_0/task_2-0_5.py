27933ed9ae5153f0e595b8e06f98efa709b5b7b5
Notalp <notalp162@gmail.com>
2026-02-17 17:19:36 +0300

    projects_2

---
+++ projects_2/task_2_0/task_2-0_5.py
@@ -0,0 +1,4 @@
+name = "Виталий"
+group = 202
+score = 52
+print(f"Студент {name} из группы {group} получил {score} баллов.")
---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,3 @@
+f = open("output.txt", "w", encoding="utf-8")
+print("Платон 8 лет, группа 202, 167 см, вес 228 кг", file=f)
+f.close()
