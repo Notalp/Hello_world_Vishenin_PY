249fd23d3056537c247521ee31de97029ff329c2
Notalp <notalp162@gmail.com>
2026-06-03 20:57:48 +0300

    Delete projects_2 directory

--- projects_2/task_2_0/task_2-0_6.py
+++
@@ -1,3 +0,0 @@
-f = open("output.txt", "w", encoding="utf-8")
-print("Платон 8 лет, группа 202, 167 см, вес 228 кг", file=f)
-f.close()
--- projects_2/task_2_2/task_2-2_3.py
+++
@@ -1,9 +0,0 @@
-# Инициализация переменных (snake_case)
-device_name = "Микроскоп"
-inventory_number = "INV-2024-001"
-is_working = "исправен"
-quantity = 3
-
-# Вывод таблицы с использованием табуляции
-print("Название\tИнвентарный №\tСостояние\tКоличество")
-print(f"{device_name}\t{inventory_number}\t{is_working}\t{quantity}")
