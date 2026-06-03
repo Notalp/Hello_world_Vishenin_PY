b1a1d839af058843c7f9a358580cc704180a6055
Notalp <notalp162@gmail.com>
2026-06-03 20:57:58 +0300

    Delete projects_3 directory

--- projects_3/task_3_13/task_3-13-1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Замена старого пути на новый с помощью sed (альтернативный разделитель |)
-sed -i 's|/var/lib/mysql/data|/mnt/ssd/mysql|g' settings.php
--- projects_3/task_3_14/task_3-14_students.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "=== Имена студентов ==="
-awk '{print $1}' students.txt
-
-echo -e "\n=== Оценки студентов ==="
-awk '{print $2}' students.txt
-
-echo -e "\n=== Номер строки и имя ==="
-awk '{print NR, $1}' students.txt
